6e80ff7bd1844fd188e7fd61f50fd4ed265ef182
Leandro Medeiros <leandromedeiros@Leandros-MacBook-Air-2.local>
2026-03-04 11:39:59 -0300

    chore: remove test files

--- test-file.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function TestComponent() {
-  const badVariable = "using single quotes and var";
-
-  const unused = "this will trigger warning";
-
-  return <div>{badVariable}</div>;
-}
